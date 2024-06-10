34ce6aa1ca6ef7d7d9a7ea7d15a6c5e8a8035925
zenuie <30676398+zenuie@users.noreply.github.com>
2024-06-10 11:30:10 +0800

    [Add] adapter and exceptions

--- payment_gateway_sdk/core/adapter.py
+++ payment_gateway_sdk/core/adapter.py
@@ -1,4 +1,16 @@
-import requests
-from abc import ABC, abstractmethod
+from payment_gateway_sdk.core.exceptions import ValidationError
 
 
+class BaseAdapter:
+    # error_messages  = {}
+
+    def __init__(self, request):
+        self.request = request
+
+    def validation_error(self, code, *args):
+        # get error messages
+        message = self.error_messages[code]
+        if args:
+            message = message % args
+        exc = ValidationError(message, code=code)
+        return exc
---
+++ payment_gateway_sdk/core/exceptions.py
@@ -0,0 +1,5 @@
+class ValidationError(Exception):
+    def __init__(self, message, code=None):
+        super().__init__(message)
+        self.message = message
+        self.code = code
